18905db0c013ac9dc1bf3139f2ecb973d3017595
duongddat <duongvandat080602@gmail.com>
2023-01-05 16:29:04 +0700

    Sidebar-2

---
+++ src/components/SuggestedAccounts/AccountPreview/AccountPreview.js
@@ -0,0 +1,39 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import classNames from 'classnames/bind';
+
+import Button from '~/components/Button';
+import styles from './AccountPreview.module.scss';
+
+const cx = classNames.bind(styles);
+function AccountPreview() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('header')}>
+                <img
+                    className={cx('avatar')}
+                    src="https://mystickermania.com/cdn/stickers/genshin-impact/genshin-zhongli-meditation-512x512.png"
+                    alt=""
+                />
+                <Button className={cx('follow-btn')} primary>
+                    Follow
+                </Button>
+            </div>
+            <div className={cx('body')}>
+                <p className={cx('nickname')}>
+                    <strong>zhongli</strong>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <p className={cx('name')}>Morax</p>
+                <p className={cx('analytics')}>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Followers</span>
+                    <strong className={cx('value')}>8.2M </strong>
+                    <span className={cx('label')}>Likes</span>
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default AccountPreview;
